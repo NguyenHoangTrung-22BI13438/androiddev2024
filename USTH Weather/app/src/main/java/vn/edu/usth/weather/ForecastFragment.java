f6d3694149f31ff05c6dd9388e4e3969aec6e21d
Thao Hoang <trungnguyenhoang2211@gmail.com>
2024-08-28 14:54:15 +0700

    prac4

--- USTH Weather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -1,12 +1,14 @@
 package vn.edu.usth.weather;
+
 import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,12 +17,9 @@ import android.view.ViewGroup;
  */
 public class ForecastFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +27,6 @@ public class ForecastFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForecastFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ForecastFragment newInstance(String param1, String param2) {
         ForecastFragment fragment = new ForecastFragment();
         Bundle args = new Bundle();
@@ -58,16 +48,16 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        FrameLayout view = (FrameLayout) inflater.inflate(R.layout.fragment_forecast, container, false);
+        view.setBackgroundColor(Color.BLUE);
 
-        // Set the background color of the view
-        view.setBackgroundColor(Color.parseColor("#20FF0000")); // Example with red color
+        LinearLayout linearLayout = new LinearLayout(view.getContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        TextView textView = new TextView(view.getContext());
 
-        // You can set other colors as needed
-        // view.setBackgroundColor(Color.parseColor("#2000FF00")); // Example with green color
-        // view.setBackgroundColor(Color.parseColor("#200000FF")); // Example with blue color
 
+        view.addView(linearLayout);
+        linearLayout.addView(textView);
         return view;
     }
 }
